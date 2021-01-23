dd0ed6f24ead988f73becd5518f4bf2a61e10a93
Inant <inant.kharisma@gmail.com>
2021-01-23 17:45:04 +0700

    crud customer

---
+++ resources/views/penjualan/customer/edit-customer.blade.php
@@ -0,0 +1,83 @@
+@extends('common.template')
+@section('container')
+
+<div class="card shadow py-2">
+  <div class="card-body">
+    @if (session('status'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('status') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    @if (session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('error') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    <a href="{{$btnRight['link']}}" class="btn btn-primary mb-3"> <span class="fa fa-arrow-alt-circle-left"></span> {{$btnRight['text']}}</a>
+    <hr>
+    <form action="{{ route('customer.update', $customer->kode_customer) }}" method="POST">
+      @csrf
+      @method('put')
+      <label>Kode Customer</label>
+      <input type="text" class="form-control {{ $errors->has('kode_customer') ? ' is-invalid' : '' }}" value="{{ old('kode_customer', $customer->kode_customer) }}" autofocus name="kode_customer" placeholder="ex : customer 1" readonly>
+      @if ($errors->has('kode_customer'))
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('kode_customer') }}</strong>
+          </span>
+      @endif
+
+      <br>
+
+      <label>Nama</label>
+      <input type="text" class="form-control {{ $errors->has('nama') ? ' is-invalid' : '' }}" value="{{ old('nama', $customer->nama) }}" autofocus name="nama" placeholder="ex : Anthony Davis">
+      @if ($errors->has('nama'))
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('nama') }}</strong>
+          </span>
+      @endif
+
+      <br>
+
+      <label>Alamat</label>
+      <textarea class="form-control" name="alamat" id="alamat" cols="30" rows="5">{{old('alamat', $customer->alamat)}}</textarea>
+      @if ($errors->has('alamat'))
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('alamat') }}</strong>
+          </span>
+      @endif
+
+      <br>
+
+      <label>No Hp</label>
+      <input type="text" class="form-control {{ $errors->has('no_hp') ? ' is-invalid' : '' }}" value="{{ old('no_hp', $customer->no_hp) }}" autofocus name="no_hp" placeholder="ex : 085331xxxxxx">
+      @if ($errors->has('no_hp'))
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('no_hp') }}</strong>
+          </span>
+      @endif
+
+      <br>
+
+      <label>Piutang</label>
+      <input type="text" class="form-control {{ $errors->has('piutang') ? ' is-invalid' : '' }}" value="{{ old('piutang', $customer->piutang) }}" autofocus name="piutang" placeholder="ex : 1.000.000,00">
+      @if ($errors->has('piutang'))
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $errors->first('piutang') }}</strong>
+          </span>
+      @endif
+
+      <br>
+
+      <button type="reset" class="btn btn-default"> <span class="fa fa-times"></span> Cancel</button>
+
+      <button type="submit" class="btn btn-primary"> <span class="fa fa-save"></span> Save</button>
+    </form>
+  </div>
+</div>
+@endsection
---
+++ resources/views/penjualan/customer/list-customer.blade.php
@@ -0,0 +1,87 @@
+@extends('common.template')
+@section('container')
+<div class="card shadow py-2">
+    <div class="card-body">
+        {{-- <div class="col-12"> --}}
+        @if (session('status'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('status') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+        {{-- </div> --}}
+        <div class="row">
+          <div class="col-2">
+            <a href="{{$btnRight['link']}}" class="btn btn-primary mb-3"> <span class="fa fa-plus-circle"></span> {{$btnRight['text']}}</a>
+          </div>
+          <div class="col-auto ml-auto">
+            <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="{{ route('customer.index') }}" method="get">
+              <div class="input-group">
+                <input type="text" class="form-control bg-light border-1 small" placeholder="Cari Data..." aria-label="Search" name="keyword" aria-describedby="basic-addon2" value="{{Request::get('keyword')}}">
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="submit">
+                    <i class="fas fa-search fa-sm"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped table-hover table-bordered table-custom">
+                <thead>
+                    <tr>
+                        <td>#</td>
+                        <td>Kode Customer</td>
+                        <td>Nama</td>
+                        <td>Alamat</td>
+                        <td>No. Hp</td>
+                        <td>Piutang</td>
+                        <td>Opsi</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                        $page = Request::get('page');
+                        $no = !$page || $page == 1 ? 1 : ($page - 1) * 10 + 1;
+                    @endphp
+                    @foreach ($customer as $value)
+                        <tr>
+                            <td>{{$no}}</td>
+                            <td>{{$value->kode_customer}}</td>
+                            <td>{{$value->nama}}</td>
+                            <td>{{$value->alamat}}</td>
+                            <td>{{$value->no_hp}}</td>
+                            <td class="text-right">Rp. {{number_format($value->piutang, 2, ',','.')}}</td>
+                            <td class="text-center">
+                                <div class="dropdown dropdown-link">
+                                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                        Opsi
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a href="{{ route('customer.edit', $value->kode_customer) }}" class="dropdown-item">{{ __('Edit') }}</a>
+                                        {{-- <form action="{{ route('Customer.destroy', $value->kode_customer) }}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="button" class="ml-1 dropdown-item" onclick="confirm('{{ __("Apakah anda yakin ingin menghapus?") }}') ? this.parentElement.submit() : ''">
+                                                {{ __('Hapus') }}
+                                            </button>
+                                        </form>   --}}
+                                    </div>
+                                </div>
+
+                            </td>
+                        </tr>
+                        @php
+                            $no++
+                        @endphp
+                    @endforeach
+                </tbody>
+            </table>
+            {{$customer->appends(Request::all())->links('vendor.pagination.custom')}}
+        </div>
+    </div>
+</div>
+@endsection
